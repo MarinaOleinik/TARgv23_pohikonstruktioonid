3883ce3fdb363a5f28b80fba39f1dfc34779e2d4
marinaoleinik <marina.oleinik@tthk.ee>
2023-11-16 12:38:32 +0200

    tehed

--- TARgv23_pohikonstruktioonid.py
+++ TARgv23_pohikonstruktioonid.py
@@ -9,3 +9,12 @@ print("Tere tulemast kooli",kool.lower(),"!\nOle hea",kursus,".kuursuse õpilase
 print("Tere tulemast kooli {0}!\nOle hea {1}.kuursuse õpetajaks!".format(kool,kursus))#kool="Tthk"
 print(type(kool))
 print(type(kursus))
+arv1=float(input("Arv 1: "))
+arv2=float(input("Arv 2:"))
+print("{0} + {1} = {2}".format(arv1,arv2,arv1+arv2)) #summa
+print("{0} - {1} = {2}".format(arv1,arv2,arv1-arv2)) #lahutamine
+print("{0} * {1} = {2}".format(arv1,arv2,arv1*arv2)) #korrutis
+print("{0} / {1} = {2}".format(arv1,arv2,arv1/arv2)) #jagamine
+print("{0} astmes {1} = {2}".format(arv1,arv2,arv1**arv2)) #astendamine
+print("{0} ja {1} jääk = {2}".format(arv1,arv2,arv1%arv2)) #jagamisjääk
+print("{0} ja {1} jagamise täis osa {2}".format(arv1,arv2,arv1//arv2))
