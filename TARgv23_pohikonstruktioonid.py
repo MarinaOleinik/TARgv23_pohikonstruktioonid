331633ff866e3e9bbe5ddc12962ec04e38f79079
marinaoleinik <marina.oleinik@tthk.ee>
2023-11-16 15:12:28 +0200

    Ülesanne Funktsioonide print(),input(), float(), int() kasutamine

--- TARgv23_pohikonstruktioonid.py
+++ TARgv23_pohikonstruktioonid.py
@@ -1,3 +1,55 @@
+from math import * # matemaatilised funktsioonid
+#Ülesanne "Funktsioonide print(),input(), float(), int() kasutamine"
+
+# 1
+#print("Tere, maailm!")
+#nimi=input("Mis on sinu nimi?")
+#print("Tere, maailm! Tervitan sind {0}!".format(nimi))
+#vanus=int(input("Kui vana sa oled"))
+#print("Tere, maailm! Tervitan sind {0}! Sa oled {1} aastat vana.".format(nimi, vanus))
+
+#2
+#vanus = 18 #type(vanus) -> int
+#eesnimi = "Jaak" #str
+#pikkus = 16.5 #float
+#kas_käib_koolis = True #bool
+
+#3
+#kommide_arv=int(input("Laual olevate kommide arv: "))
+#print("Laua peal on {0}".format(kommide_arv))
+#mitu=int(input("Mitu kommi sa soovid süüa: "))
+#kommide_arv-=mitu
+#print("Nüüd laua peal on ",kommide_arv)
+
+#4
+#C=float(input("Ümbermõõt: ")) # C=2*pi*r=pi*d
+#d=round(C/pi,2) #import math -> math.pi
+#print("Läbimõõt: {0}".format(round(d,2)))
+
+#5
+#a=float(input("Esimene kateet"))
+#b=float(input("Teine kateet"))
+#d=sqrt(a**2+b**2)
+#d1=hypot(a,b)
+#print("d=",d)
+#print("d1=",d1)
+
+#6
+try:
+    aeg = float(input("Mitu tundi kulus sõiduks? "))
+    teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+    kiirus = aeg / teepikkus
+    print("Sinu kiirus oli " + str(kiirus) + " km/h")
+except :
+    print("Andmetüübi viga!")
+
+
+
+
+
+
+
+#Esimene osa:
 print("Tere tulemast!".center(50))
 kool=input("\tMis koolis sa õpid?: ").capitalize() #str Kool
 kursus=int(input("\tMis kuursusel?: ")) #int kursus
@@ -18,3 +70,8 @@ print("{0} / {1} = {2}".format(arv1,arv2,arv1/arv2)) #jagamine
 print("{0} astmes {1} = {2}".format(arv1,arv2,arv1**arv2)) #astendamine
 print("{0} ja {1} jääk = {2}".format(arv1,arv2,arv1%arv2)) #jagamisjääk
 print("{0} ja {1} jagamise täis osa {2}".format(arv1,arv2,arv1//arv2))
+
+tehe=input("Mida teha: ")
+v=eval(str(arv1)+tehe+str(arv2))
+print(v)
+
