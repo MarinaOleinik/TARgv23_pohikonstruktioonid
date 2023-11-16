27c6896630d0c543b5f53672e981a2b8a1f62164
marinaoleinik <marina.oleinik@tthk.ee>
2023-11-16 12:35:10 +0200

    aritmeetilised tehd

--- TARgv23_pohikonstruktioonid.py
+++ TARgv23_pohikonstruktioonid.py
@@ -1,6 +1,11 @@
-print("Tere tulemast!")
-kool=input("Mis koolis sa õpid?: ") #str kool
-kursus=int(input("Mis kuursusel?: ")) #int kursus
-print("Tere tulemast kooli "+kool+"!\nOle hea "+str(kursus)+".kuursuse õpilaseks!")
-print("Tere tulemast kooli",kool,"!\nOle hea",kursus,".kuursuse õpilaseks!")
-print("Tere tulemast kooli {0}!\nOle hea {1}.kuursuse õpetajaks!".format(kool,kursus))
+print("Tere tulemast!".center(50))
+kool=input("\tMis koolis sa õpid?: ").capitalize() #str Kool
+kursus=int(input("\tMis kuursusel?: ")) #int kursus
+
+print("Tere tulemast kooli "+kool.upper()+"!\nOle hea "+str(kursus)+".kuursuse õpilaseks!") #kool="TTHK"
+
+print("Tere tulemast kooli",kool.lower(),"!\nOle hea",kursus,".kuursuse õpilaseks!")#kool="tthk"
+
+print("Tere tulemast kooli {0}!\nOle hea {1}.kuursuse õpetajaks!".format(kool,kursus))#kool="Tthk"
+print(type(kool))
+print(type(kursus))
