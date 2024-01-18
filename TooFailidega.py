d74962a3c87c3315247fe93ecedf5f6d317c6948
marinaoleinik <marina.oleinik@tthk.ee>
2024-01-18 16:24:33 +0200

    text to speech

--- TooFailidega.py
+++ TooFailidega.py
@@ -1,18 +1,21 @@
 def Loe_failist(fail:str)->list:
     jarjend=[]
+    raagi=""
     f=open(fail,'r',encoding="utf-8")
     for rida in f:
         jarjend.append(rida.strip())
+        raagi+=rida
     f.close()
-    return jarjend
+    return jarjend,raagi
 
 def Kirjuta_failisse(fail:str,jarjend:list):
     f=open(fail,'w',encoding="utf-8")
     for item in jarjend:
         f.write(item+'\n')
+        
     f.close()
-
-paevad=Loe_failist("Paevad.txt")
+test_to_speech=""
+paevad, test_to_speech=Loe_failist("Paevad.txt")
 print(paevad)
 
 list_=[]
@@ -23,8 +26,15 @@ Kirjuta_failisse("Nimed.txt",list_)
 
 with open('Nimed.txt', 'r') as f:
     print(f.read())
-
-#from genericpath import isfile
+    
 from os import *
-if path.isfile("Nimi.txt"):
-    remove("Nimed.txt")
+if path.isfile("Nimi.txt"):   #path.isdir(kaust)
+    remove("Nimed.txt") #faili kustutamine, kui ta on olemas
+    from gtts import gTTS
+
+from gtts import gTTS
+def Heli(text:str,keel:str):
+    obj=gTTS(text=text,lang=keel,slow=False).save("heli.mp3")
+    system("heli.mp3")
+
+Heli(test_to_speech,"et")
